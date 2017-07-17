9d1ea2da5420226daa63c38c8427112187b56bda
Siddharth RoyChoudhury <debu2in@gmail.com>
2017-07-17 20:26:12 +0530

    Added class to database

--- server/database/database.js
+++ server/database/database.js
@@ -1,44 +1,69 @@
 var MongoClient = require('mongodb').MongoClient;
 var Dbprops = require("./dbprops");
 //internal objects to hide implementations
-
-function DbConn(dbURL){
-  this.dbURL = dbURL;
+function DbConn() {
+  //this.dbprops = new Dbprops()
+  this.dbURL = this.dbprops.getDbURL();
 }
-DbConn.prototype.connect = function(dBparams,dbCRUDAction) {
-    MongoClient.connect(this.dbURL, function(err, dbConnection) {
-        dbCRUDAction(dbConnection,dBparams)
-    }) 
+DbConn.prototype.connect = function(dBparams, dbCRUDAction) {
+  MongoClient.connect(this.dbURL, function(err, dbConnection) {
+    dbCRUDAction(dbConnection, dBparams)
+  })
 }
-function Database(){
-  this.dbprops = new Dbprops()
-  this.dbConn = new DbConn(this.dbprops.getDbURL());
-}
-
-Database.prototype.read = function(readParams){
-   this.dbConn.connect(readParams,find);
-}
-function find(dbConnection,dBparams){
-   var collection = dbConnection.collection(dBparams.collectionName);
-   collection.find(dBparams.dBData.criteria,dBparams.dBData.projection,dBparams.dBData.options)
-   .toArray(function(error,docs){
-    if(error == undefined || error == null){
-    	console.log("Docs "+JSON.stringify(docs,null,2));
-    	dBparams.callback(docs);
-
+class Database {
+  constructor() {
+    this.dbConn = new DbConn();
+    this.findHandler = function(dbConnection, dBparams) {
+      find(dbConnection, dBparams)
     }
+    this.createHandler = function(dbConnection, dBparams) {
+      insert(dbConnection, dBparams);
     }
-   )
+
+    function find(dbConnection, dBparams) {
+      var collection = dbConnection.collection(dBparams.collectionName);
+      collection.find(dBparams.dBData.criteria, dBparams.dBData.projection, dBparams.dBData.options).toArray(function(error, docs) {
+        if (error == undefined || error == null) {
+          console.log("Docs " + JSON.stringify(docs, null, 2));
+          dbConnection.close()
+          dBparams.callback(docs);
+        }
+      })
+    }
+
+    function insert(databaseConnection, dbCreateParams) {
+      var collection = databaseConnection.collection(dbCreateParams.collectionName);
+      collection.insert(dbCreateParams.dbData,
+        function(err, result) {
+          var responseObject = ErrorAndResponseWrapper.wrapResponseAndError(err, result);
+          databaseConnection.close();
+          dbCRUDParams.callback(responseObject, dbCRUDParams.theThisInstance);
+        });
+    }
+    function cleanUp(){
+      databaseConnection.close();
+      dbCRUDParams.callback(responseObject, dbCRUDParams.theThisInstance);
+    }
+  }
+  read(readParams) {
+    this.dbConn.connect(readParams, this.findHandler);
+  }
+  create(createParams) {
+    this.dbConn.connect(createParams, this.createHandler);
+  }
+
 }
-Database.prototype.create = function(createParams){
-    this.dbConn.connect(createParams,insert);
+
+
+
+Database.prototype.create = function(createParams) {
+
 }
-function insert(){
-    
+
+function insert() {
+
 }
 //Create
 //Delete
 //Update
 module.exports = Database;
- 
-    
